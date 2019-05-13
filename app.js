d40f960c0dc80bca68d7a05d87dddbafb072bd24
Jaalcampos <javi.campos.gomez@gmail.com>
2019-05-13 09:46:19 -0600

    Usuario y Token listo

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose')
+var bodyParser = require('body-parser');
 
 // Inicializar variables
-var aap = express();
+var app = express();
+
+// Body-Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+// Importar Rutas
+var appRoute = require('./routes/app');
+var usuarioRoute = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -13,15 +26,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online')
 });
 
-// Rutas
-aap.get('/',(req, res, next) => {
+// Routas
+app.use('/usuario',usuarioRoute);
+app.use('/login',loginRoutes);
+app.use('/',appRoute);
 
-    res.status(200).json({
-        ok:true,
-        mensaje: 'peticion realizada correctamente'
-    })
-
-})
 
 // Escuchar peticiones
-aap.listen(3000,()=> { console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online') });
+app.listen(3000,()=> { console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online') });
---
+++ routes/app.js
@@ -0,0 +1,16 @@
+// Requires
+var express = require('express');
+
+var app = express();
+
+// Rutas
+app.get('/',(req, res, next) => {
+
+    res.status(200).json({
+        ok:true,
+        mensaje: 'peticion realizada correctamente'
+    })
+
+});
+
+module.exports = app;
